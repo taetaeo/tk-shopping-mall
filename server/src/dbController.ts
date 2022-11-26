97691175730ffe5285813ddbb02ae26101bcc0ed
ohtaekwon <ohtk1119@gmail.com>
2022-11-27 05:23:22 +0900

    feat: firebase - express - apolloserver 연동

---
+++ server/src/dbController.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { resolve } from "path";
+
+export enum DBField {
+  EVENT = "events"
+}
+
+const basePath = resolve(); // __dirname
+
+const fileNames = {
+  [DBField.EVENT]: resolve(basePath, "./src/db/events.json"),
+};
+
+export const readDB = (target: DBField) => {
+  try {
+    return JSON.parse(
+      fs.readFileSync(fileNames[target], { encoding: "utf-8" })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const writeDB = (target: DBField, data: any) => {
+  try {
+    fs.writeFileSync(fileNames[target], JSON.stringify(data, null, "  "));
+  } catch (err) {
+    console.log(err);
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,35 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import schema from "./schema";
+import resolvers from "./resolvers";
+import { DBField, readDB } from "./dbController";
 
-const app = express();
-app.listen({ port: 8000 });
-console.log("server is listening on port 8000....");
+const ENDPOINT = "http://localhost:3000";
+const APOLLO_STUDIO = "https://studio.apollographql.com";
+
+(async () => {
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers: resolvers,
+    context: {
+      db: {
+        eventItems: readDB(DBField.EVENT),
+      },
+    },
+  });
+
+  const app = express();
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+    cors: {
+      origin: ["http://localhost:3000", "https://studio.apollographql.com"],
+      credentials: true,
+    },
+  });
+  await app.listen({ port: 8000 });
+  // console.log("resolvers", resolvers);
+  // console.log("schema", schema);
+  console.log("server is listening on port 8000....");
+})();
