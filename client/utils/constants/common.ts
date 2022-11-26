f58e6bebff13e545cee838f4f575f883585f51e3
ohtaekwon <ohtk1119@gmail.com>
2022-11-26 22:21:33 +0900

    feat: icons및 common 상수

---
+++ client/utils/constants/common.ts
@@ -0,0 +1,21 @@
+export const SIZE = {
+  X_SMALL: "xs",
+  SMALL: "sm",
+  MIDDLE: "md",
+  LARGE: "lg",
+} as const;
+export const DIRECTION = {
+  COLUMN: "column",
+  ROW: "row",
+} as const;
+export const POSITION = {
+  LEFT: "left",
+  RIGHT: "right",
+} as const;
+export const VARIANTS_NAME = {
+  SUCCESS: "success",
+  ERROR: "error",
+  WARNING: "warning",
+  DEFAULT: "default",
+  DEFAULT_LIGHT: "default_light",
+} as const;
