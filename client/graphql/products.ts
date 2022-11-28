c07a47785e726c1bf5f0d7a32a00c3de5712e76f
ohtaekwon <ohtk1119@gmail.com>
2022-11-29 02:56:30 +0900

    chore: client에서 addProduct graphql schema

--- client/graphql/products.ts
+++ client/graphql/products.ts
@@ -9,7 +9,6 @@ export const GET_PRODUCTS = gql`
       image_url
       origin_price
       discount
-      new
       createdAt
       category {
         category_lg
@@ -29,7 +28,6 @@ export const GET_PRODUCT = gql`
       image_url
       origin_price
       discount
-      new
       createdAt
       category {
         category_lg
@@ -56,13 +54,44 @@ export const GET_SELECTED_PRODUCT = gql`
       image_url
       origin_price
       discount
-      new
       createdAt
       category {
         category_lg
         category_md
         category_sm
       }
+    }
+  }
+`;
+export const ADD_PRODUCT = gql`
+  mutation ADD_PRODUCT(
+    $brand: String!
+    $name: String!
+    $image_url: String!
+    $origin_price: Int!
+    $discount: Int!
+    $category_lg: String!
+    $category_md: String!
+    $category_sm: String!
+  ) {
+    addProduct(
+      brand: $brand
+      name: $name
+      image_url: $image_url
+      origin_price: $origin_price
+      discount: $discount
+      category_lg: $category_lg
+      category_md: $category_md
+      category_sm: $category_sm
+    ) {
+      brand
+      name
+      image_url
+      origin_price
+      discount
+      category_lg
+      category_md
+      category_sm
     }
   }
 `;
