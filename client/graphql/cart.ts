cac1de34cbc8659a7d07e9d64bee04ab46d14994
ohtaekwon <ohtk1119@gmail.com>
2022-11-30 03:18:50 +0900

    feat: client에서의 GET/ADD CART Graphql Schema구현

---
+++ client/graphql/cart.ts
@@ -0,0 +1,47 @@
+import { gql } from "graphql-tag";
+
+export const GET_CARTS = gql`
+  query GET_CARTS {
+    cart {
+      id
+      amount
+      product {
+        id
+        brand
+        name
+        image_url
+        origin_price
+        discount
+        createdAt
+        category {
+          category_lg
+          category_md
+          category_sm
+        }
+      }
+    }
+  }
+`;
+export const ADD_CART = gql`
+  Mutation ADD_CART($id:ID!, $count:Int){
+    addCart(productId:$id, count:$count){
+      id
+      amount
+      product {
+        id
+        brand
+        name
+        image_url
+        origin_price
+        discount
+        createdAt
+        category {
+          category_lg
+          category_md
+          category_sm
+        }
+      }
+    }
+  }
+
+`;
