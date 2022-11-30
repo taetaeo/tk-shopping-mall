81389501095730ed029b373640186b082c41243f
ohtaekwon <ohtk1119@gmail.com>
2022-12-01 05:03:55 +0900

    feat: 검색을 통한 추천 검색결과

--- server/src/resolvers/search.ts
+++ server/src/resolvers/search.ts
@@ -19,24 +19,42 @@ import {
 const searchResolver: Resolver = {
   Query: {
     searchItems: async (parent, { keyword = "" }) => {
-      const searchItems = collection(db, "products");
-      const queryOptions = [orderBy("createdAt", "desc")];
-
       if (!keyword) throw Error("검색한 브랜드 정보가 없습니다.");
-      queryOptions.unshift(where("brand", "==", keyword)); // category_sm에 따라 long, short 등 분리
-      // queryOptions.unshift(where("brand", "<=", keyword));
-      // queryOptions.unshift(where("brand", ">=", keyword + "\uf8ff"));
-      const q = query(searchItems, ...queryOptions);
-      const querySnapshot = await getDocs(q);
+      const newKeyword = keyword.replace(/\s/g, "");
+      const queryOptions = [orderBy("createdAt", "desc")];
+      const newData = [];
+
+      const searchBrand = collection(db, "products");
+      const brandSnapshot = await getDocs(searchBrand);
       const data: DocumentData[] = [];
-      querySnapshot.forEach((doc: DocumentData) => {
+      brandSnapshot.forEach((doc: DocumentData) => {
         const d = doc.data();
-        data.push({
+        if (!data.includes(d.brand)) {
+          data.push(d.brand);
+        }
+      });
+      for (const d of data) {
+        const newD = d.replace(/\s/g, "");
+        if (newD.includes(newKeyword)) {
+          newData.push(d);
+        }
+      }
+
+      for (const d of newData) {
+        queryOptions.unshift(where("brand", "==", d));
+      }
+
+      const q = query(searchBrand, ...queryOptions);
+      const searchSnapshot = await getDocs(q);
+      const searchData: DocumentData[] = [];
+      searchSnapshot.forEach((doc: DocumentData) => {
+        const d = doc.data();
+        searchData.push({
           id: doc.id,
           ...d,
         });
       });
-      return data;
+      return searchData;
     },
     searchBrand: async (parent, { keyword }) => {
       const newKeyword = keyword.replace(/\s/g, "");
