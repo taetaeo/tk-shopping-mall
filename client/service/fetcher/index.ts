a13ff9b448f1c0c49256c2bbb2eafa0f370f21f6
ohtaekwon <ohtk1119@gmail.com>
2023-03-14 21:28:07 +0900

    env에 배포한 서버로 추가

--- client/service/fetcher/index.ts
+++ client/service/fetcher/index.ts
@@ -1,6 +1,6 @@
 import { request, RequestDocument } from "graphql-request";
 
-const END_POINT = "http://localhost:8000/graphql";
+const END_POINT = process.env.NEXT_PUBLIC_SERVER_URL + "/graphql";
 
 const graphQLFetcher = (query: RequestDocument, variables = {}) =>
   request(END_POINT, query, variables);
